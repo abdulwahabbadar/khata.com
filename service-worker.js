15eff7aa5ba90b4577350c87480e0b2d7e4c46ad
Abdul Wahab Badar <abdulwahabbadar786@gmail.com>
2026-09-23 12:43:19 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE = "khata-v1";
+const FILES = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Network first, cache fallback: updates aayein to foran nazar aayein, offline bhi chale
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match("./")))
+  );
+});
